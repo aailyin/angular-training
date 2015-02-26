5717a52c87cf13f1ed01dfd6ff9b3f8892cb97f9
Anton Ilyin <AntonLlyin@landsend.com>
2015-02-26 17:27:29 +0300

    created new files

---
+++ labs/lab2/lab/js/personGridController.js
@@ -0,0 +1,35 @@
+(function () {
+    angular
+        .module('personLibraryApp')
+        .controller('PersonGridController', PersonGridController);
+
+    PersonGridController.$inject = ['$scope', 'PersonService'];
+
+    function PersonGridController($scope, PersonService) {
+        $scope.query = '';
+        $scope.persons = PersonService.getItems();
+        $scope.phoneType = 'home';
+        $scope.predicate = '';
+        $scope.reverse = false;
+        $scope.showButton = {
+            'text': 'Show Lesson Content',
+            'btnClass': 'btn-default',
+            'isShow': false
+        };
+
+        $scope.changeBtn = function () {
+            if($scope.showButton.isShow){
+                $scope.showButton.btnClass = 'btn-default';
+                $scope.showButton.text = 'Show Lesson Content';
+                $scope.showButton.isShow = false;
+            } else {
+                $scope.showButton.btnClass = 'btn-primary';
+                $scope.showButton.text = 'Hide Lesson Content';
+                $scope.showButton.isShow = true;
+            }
+            $scope.predicate = '';
+        };
+    };
+})();
+
+
